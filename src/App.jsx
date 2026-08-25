8c8068ee834b14cc8777a361bd85b014e13147dd
gyeonggumun <lion989072@gmail.com>
2026-08-25 16:57:42 +0900

    새로운 페이지 라우트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Dashboard from './pages/Dashboard';
 import Transactions from './pages/Transactions';
-import Budget from './pages/Budget'; // 1. 예산 페이지 불러오기
+import Budget from './pages/Budget';
+import Statistics from './pages/Statistics';
+import Goals from './pages/Goals'; 
 
 function App() {
   return (
@@ -11,8 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/transactions" element={<Transactions />} />
-          <Route path="/budget" element={<Budget />} /> {/* 2. 예산 페이지 라우팅 추가 */}
-          {/* 차후 Statistics, Goals, Settings 라우팅을 여기에 추가합니다 */}
+          <Route path="/budget" element={<Budget />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/goals" element={<Goals />} />
         </Routes>
       </Layout>
     </BrowserRouter>
